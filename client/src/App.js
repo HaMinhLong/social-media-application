cb7c4f11f757fa74f82b175a392d7a9ee2e54fa2
Minh Long <haminhlong3@gmail.com>
2021-01-21 16:05:53 +0000

    authentication context

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,32 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
+
+import { Container } from "semantic-ui-react";
+
+import { AuthProvider } from "./context/auth";
+
+import MenuBar from "./components/MenuBar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+import AuthRoute from "./util/AuthRoute";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <AuthProvider>
+        <Router>
+          <Container>
+            <MenuBar />
+            <AuthRoute exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </Container>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
---
+++ client/src/pages/Home.js
@@ -0,0 +1,31 @@
+import React, { useContext, useState } from "react";
+import { useQuery } from "@apollo/client";
+import { Grid, Transition } from "semantic-ui-react";
+import PostCard from "../components/PostCard.js";
+import { FETCH_POSTS_QUERY } from "../util/graphql";
+
+function Home() {
+  const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+
+  return (
+    <Grid columns={3}>
+      <Grid.Row className="page-title">
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <h1>Loading posts..</h1>
+        ) : (
+          data.getPosts &&
+          data.getPosts.map((post) => (
+            <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+              <PostCard post={post} />
+            </Grid.Column>
+          ))
+        )}
+      </Grid.Row>
+    </Grid>
+  );
+}
+
+export default Home;
